498af9f28cbe7efc53608f7d387bcc40b5ff1ee8
Michel Frechette <mifrec@hotmail.com>
2016-03-08 21:31:20 -0500

    echo, html, test for php

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
-<!-- http://cafeteriacafe.com/boston-youtube-php/ -->
-
+<!-- http://cafeteriacafe.com/index.php -->
 <?php
-  phpinfo();
+
+  echo 'echo hello from inside php tags'
+
  ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>index</title>
+  </head>
+  <body>
+    <h2>The New Boston YouTube Video PHP Series</h2>
+  </body>
+</html>
---
+++ test_for_php.php
@@ -0,0 +1,5 @@
+<!-- http://cafeteriscafe.com/boston-youtube-php/test_for_php.php -->
+
+<?php
+  phpinfo();
+ ?>
